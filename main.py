2f5d3169181c4f7935cc14ee4420d676c1685bd1
Alvin Thai <alvtdev@gmail.com>
2017-06-03 20:04:21 -0700

    mash tun gui pages completed; writes user input to mashtun.txt

--- main.py
+++ main.py
@@ -1,62 +1,77 @@
 import time
 from tkinter import *
 
-filename = "output.txt"
+outputFile = "output.txt"
 bmsg = "Waiting for BK"
 afterid1 = -1;
 afterid2 = -1;
 
+#mash time var
+mTime = None
+mTemp1 = None
+mTemp2 = None 
+
 #PAGE NUMBER VAR
-#0 = start page
+#0 = menu page
 #1 = boil kettle page
 #2 = output page
 #3 = mash tun page
 #4 = MT info sent page
 pagenumber = 0;
 
-#Page navigation helper functions
-def goToStartPage():
-    pagenumber = 0
-    #start_page.pack()
-
-def goToBkPage():
-    pagenumber = 1
-    #bk_page.pack()
-    bk_page.tkraise()
-
-def goToOutPage():
-    pagenumber = 2
-    #out_page.pack()
-    printOutputs()
-    out_page.tkraise()
-
-def goToMtPage():
-    pagenumber = 3
-    #mt_page.pack()
-    mt_page.tkraise()
-
-def exitbk():
-    exit(0)
-
 #Output functions
-def printOutputsLoop():
-    ftxt = open(filename)
-    bmsg = ftxt.read()
-    ftxt.close()
-    out1.configure(text=bmsg)
-    bkui.after_cancel(afterid2)
-    afterid2 = bkui.after(1000, printOutputsLoop)
-
 def printOutputs():
-    ftxt = open(filename)
+    ftxt = open(outputFile, "r+")
     bmsg = ftxt.read()
     ftxt.close()
     out1.configure(text=bmsg)
     afterid1 = bkui.after(1000, printOutputs)
 
+#Mash Tun Menu Functions 
+def getMashData():
+    mTime = mashTimeEntry.get()
+    mTemp1 = mashTemp1Entry.get()
+    mTemp2 = mashTemp2Entry.get()
+    file = open('mashtun.txt', 'w+')
+    file.write("Mash time: %s \n" % mTime)
+    file.write("Mash temp1: %s \n" % mTemp1)
+    file.write("Mash temp2: %s \n" % mTemp2)
+    file.close()
+    #print(mTime)
+    #print(mTemp1)
+    #print(mTemp2)
+
+#Page navigation helper functions
+def goToMenuPage():
+    pagenumber = 0
+    menu_page.tkraise()
+
+def goToBkPage():
+    pagenumber = 1
+    bk_page.tkraise()
+
+def goToOutPage():
+    pagenumber = 2
+    printOutputs()
+    out_page.tkraise()
+
+def goToMtPage():
+    pagenumber = 3
+    mt_page.tkraise()
+
+def goToMtSentPage():
+    pagenumber = 4
+    getMashData()
+    mtSent_page.tkraise()
+
+def exitbk():
+    bkui.quit()
+    exit(0)
+
 
 bkui = Tk()
 bkui.title("BoilKettlePi")
+#init bkui config to account for resizing
 bkui.columnconfigure(0, weight=1)
 bkui.columnconfigure(1, weight=1)
 bkui.columnconfigure(2, weight=1) 
@@ -67,14 +82,14 @@ bkui.rowconfigure(2, weight=1)
 #bkui.rowconfigure(3, weight=1)
 
 #declare pages
-start_page = Frame(bkui)
+menu_page = Frame(bkui)
 bk_page = Frame(bkui) 
 mt_page = Frame(bkui)
 out_page = Frame(bkui)
 mtSent_page = Frame(bkui)
 
 #init config for all pages
-for frame in (start_page, bk_page, mt_page):
+for frame in (menu_page, bk_page, out_page, mt_page, mtSent_page):
     frame.grid(row=0, column=0, sticky=(N, S, E, W))
     frame.columnconfigure(0, weight=1)
     frame.columnconfigure(1, weight=1)
@@ -85,38 +100,90 @@ for frame in (start_page, bk_page, mt_page):
     frame.rowconfigure(2, weight=1)
 #    frame.rowconfigure(3, weight=1)
 
+#MENU PAGE
+mtConfig = Button(menu_page, text="MashTun Settings", command=goToMtPage)
+btConfig = Button(menu_page, text="BoilKettle Settings", command=goToBkPage)
+exitMenu = Button(menu_page, text='Exit', command=exitbk)
+menuMsg = Message(menu_page, text="Welcome to BoilKettlePi", width=10000)
 
-#exit button
-exitStart = Button(start_page, text='Exit', command=exitbk)
-exitBK = Button(bk_page, text='Exit', command=exitbk)
-exitOut = Button(out_page, text='Exit', command=exitbk)
-exitMT = Button(mt_page, text='Exit', command=exitbk)
-
-#START PAGE
-startmsg = Message(start_page, text="Welcome to BoilKettlePi", width=10000)
-MTconfig = Button(start_page, text="MashTun Settings", command=goToMtPage)
-BTconfig = Button(start_page, text="BoilKettle Settings", command=goToBkPage)
-startmsg.grid(row=2, column=1)
-MTconfig.grid(row=3, column=0)
-BTconfig.grid(row=3, column=1)
-exitStart.grid(row=3, column=2)
+mtConfig.grid(row=3, column=0)
+btConfig.grid(row=3, column=1)
+exitMenu.grid(row=3, column=2)
+menuMsg.grid(row=2, column=1)
 
 #BOIL KETTLE PAGE
-Start = Button(bk_page, text="Start Boil", command=goToOutPage)
+startBoil = Button(bk_page, text="Start Boil", command=goToOutPage)
+btsBK = Button(bk_page, text='Menu', command=goToMenuPage)
+exitBK = Button(bk_page, text='Exit', command=exitbk)
 out1 = Message(bk_page, text=bmsg, width = 10000)
 
 out1.grid(row=2, column=1)
-Start.grid(row=3, column=0)
+btsBK.grid(row=3, column=0)
+startBoil.grid(row=3, column=1)
 exitBK.grid(row=3, column=2)
 
 #output page
+exitOut = Button(out_page, text='Exit', command=exitbk)
 outmsg = Message(out_page, text=bmsg, width=10000)
-outmsg.grid(row=2, column=2)
+
+outmsg.grid(row=2, column=1)
 exitOut.grid(row=3, column=2)
 
-
 #MT PAGE
+mashTime = Label(mt_page, text="Mash Time:")
+mashTimeEntry = Entry(mt_page)
+mashTimeUnits = Label(mt_page, text="minutes")
+#mTime = mashTimeEntry.get()
+mashTemp1 = Label(mt_page, text="Mash Temperature:")
+mashTemp1Entry = Entry(mt_page)
+mashTemp1Units = Label(mt_page, text="\xb0F")
+#mTemp1 = mashTemp1Entry.get()
+mashTemp2 = Label(mt_page, text="Mash Temperature:")
+mashTemp2Entry = Entry(mt_page)
+mashTemp2Units = Label(mt_page, text="\xb0F")
+#mTemp2 = mashTemp2Entry.get()
+btsMT = Button(mt_page, text='Menu', command=goToMenuPage)
+sendMTData = Button(mt_page, text="Send to Mash Tun", command=goToMtSentPage)
+exitMT = Button(mt_page, text='Exit', command=exitbk)
+
+mashTime.grid(row=0, column=0)
+mashTimeEntry.grid(row=0, column=1) 
+mashTimeUnits.grid(row=0, column=2)
+mashTemp1.grid(row=1, column=0)
+mashTemp1Entry.grid(row=1, column=1)
+mashTemp1Units.grid(row=1, column=2)
+mashTemp2.grid(row=2, column=0)
+mashTemp2Entry.grid(row=2, column=1)
+mashTemp2Units.grid(row=2, column=2)
+btsMT.grid(row=3, column=0)
+sendMTData.grid(row=3, column=1)
 exitMT.grid(row=3, column=2)
 
-start_page.tkraise()
+#MT SENT PAGE
+btsMTSent = Button(mtSent_page, text='Menu', command=goToMenuPage)
+mtSentMsg = Message(mtSent_page, text="MT Info Sent:", width=10000)
+mtSentTime = Label(mtSent_page, text="Mash Time:")
+mtSentTimeEntry = Label(mtSent_page, text=mTime)
+mtSentTimeUnits = Label(mtSent_page, text="minutes")
+mtSentTemp1 = Label(mtSent_page, text="Mash Temperature:")
+mtSentTemp1Entry = Label(mtSent_page, text=mTemp1)
+mtSentTemp1Units = Label(mtSent_page, text="\xb0F")
+mtSentTemp2 = Label(mtSent_page, text="Mash Temperature:")
+mtSentTemp2Entry = Label(mtSent_page, text=mTemp2)
+mtSentTemp2Units = Label(mtSent_page, text="\xb0F")
+
+mtSentMsg.grid(row=0, column=1)
+mtSentTime.grid(row=1, column=0)
+mtSentTimeEntry.grid(row=1, column=1)
+mtSentTimeUnits.grid(row=1, column=2)
+mtSentTemp1.grid(row=2, column=0)
+mtSentTemp1Entry.grid(row=2, column=1)
+mtSentTemp1Units.grid(row=2, column=2)
+mtSentTemp2.grid(row=3, column=0)
+mtSentTemp2Entry.grid(row=3, column=1)
+mtSentTemp2Units.grid(row=3, column=2)
+btsMTSent.grid(row=4, column=1)
+
+
+menu_page.tkraise()
 bkui.mainloop()
